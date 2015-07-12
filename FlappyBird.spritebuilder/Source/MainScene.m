a678b08f2c1ceaf75ce54003237c68c6f82c173a
FlyBee <tobplayshd@gmail.com>
2015-07-12 21:47:48 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -168,7 +168,7 @@
     character.rotation = clampf(character.rotation, -30.f, 90.f);
     
     if (character.physicsBody.allowsRotation) {
-        float angularVelocity = clampf(character.physicsBody.angularVelocity, -1.f, 1.f);
+        float angularVelocity = clampf(character.physicsBody.angularVelocity, -0.3f, 1.f);
         character.physicsBody.angularVelocity = angularVelocity;
     }
     
